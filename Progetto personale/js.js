44025a8a38dbc08d99fcdb3a4cfd538bdca086a4
lucaBallabio <57558348+lucaBallabio@users.noreply.github.com>
2020-12-02 15:58:53 +0100

    Add files via upload

--- Progetto personale/js.js
+++ Progetto personale/js.js
@@ -1,6 +1,29 @@
-var caramelle=0;
+var vettQuantità = [];
+var vettAlimenti = [];
+var vettCosto = [];
+numEl = 0;
+var c=0;
 
-function myFunction(){
+function aggiungiTabella() {
+    for (var i = 0; i < numEl; i++) {
+        if (vettAlimenti[i] == a) {
+            vettQuantità[i] = vettQuantità[i] + 1;
+            c=1;
+        }
+    }
+
+    if (c=0) {
+        vettAlimenti[numEl] = a;
+        vettCosto[numEl] = b;
+        vettQuantità[numEl] = 1;
+        numEl++;
+        alert("ciao");
+    }
+
+    for (var i = 0; i < numEl; i++) {
+        tab += "<tr><td>" + vettAlimenti[i] + "</td><td>" + vettCosto[i] + "</td><td>" + vettQuantità[i] + "</td></tr>";
+    }
+
+    document.getElementById("tabella").innerHTML = tab;
     
-
 }
